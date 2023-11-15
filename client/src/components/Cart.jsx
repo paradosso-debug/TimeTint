6a49ec18617f10575cb024d33a907e33e68c2854
paradosso-debug <jocelin.arriaga@yahoo.com>
2023-11-14 21:00:29 -0600

    updated cart page and css

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,13 +1,102 @@
-import React from 'react'
+import React from "react";
 
 const Cart = () => {
   return (
     <>
+      <div className="cart-wrapper">
+        <div className="cart-container-first">
+          <div className="cart-info-left">
+            <div className="payment-details">
+              <h1>Payment Details</h1>
+              <h2 className="card-details">Card Details</h2>
+              <form className="payment-form">
+                <div className="input-group">
+                  <label htmlFor="name">Name</label>
+                  <input type="text" id="name" name="name" required />
+                </div>
+                <div className="input-group">
+                  <label htmlFor="cardNumber">Card Number</label>
+                  <input
+                    type="text"
+                    id="cardNumber"
+                    name="cardNumber"
+                    required
+                  />
+                </div>
+                <div className="input-group double-input">
+                  <div className="input-half">
+                    <label htmlFor="expirationDate">Expiration Date</label>
+                    <input
+                      type="text"
+                      id="expirationDate"
+                      name="expirationDate"
+                      required
+                    />
+                  </div>
+                  <div className="input-half">
+                    <label htmlFor="cvc">CVC</label>
+                    <input type="text" id="cvc" name="cvc" required />
+                  </div>
+                </div>
+                <h2 className="shipping-details">Shipping Details</h2>
+                <div className="input-group double-input">
+                  <div className="input-half">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" id="name" name="name" required />
+                  </div>
+                  <div className="input-half">
+                    <label htmlFor="mobilenumber">Mobile Number</label>
+                    <input
+                      type="text"
+                      id="mobilenumber"
+                      name="mobilenumber"
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="input-group">
+                  <label htmlFor="email">Email</label>
+                  <input type="text" id="email" name="email" required />
+                </div>
 
-    
+                <div className="input-group">
+                  <label htmlFor="address">Address</label>
+                  <input type="text" id="address" name="address" required />
+                </div>
+                <div className="input-group double-input">
+                  <div className="input-half">
+                    <label htmlFor="city">City</label>
+                    <input type="text" id="city" name="city" required />
+                  </div>
+                  <div className="input-half">
+                    <label htmlFor="zip">ZIP</label>
+                    <input type="text" id="zip" name="zip" required />
+                  </div>
+                </div>
+                <div className="input-group">
+                  <label htmlFor="country">Country</label>
+                  <input type="text" id="country" name="country" required />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+
+        <div className="cart-container-second">
+          {/* Order Details */}
+          <h2>Order Details</h2>
+          {/* ...add product details here */}
+          {/* Order Summary */}
+          <div className="order-summary">
+            <p>Subtotal: $2558</p>
+            <p>Shipping: $5.90</p>
+            <p>Total: $2563.90</p>
+            <button type="submit">Confirm Your Order</button>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Cart
-
+export default Cart;
